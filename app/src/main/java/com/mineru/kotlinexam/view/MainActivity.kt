4f61f71b51f503e76a4b3885838f05ef646b0753
Mineru98 <rmstjr1030@naver.com>
2020-02-03 01:01:58 +0900

    Update Code

---
+++ app/src/main/java/com/mineru/kotlinexam/view/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.mineru.kotlinexam.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mineru.kotlinexam.R
+import com.mineru.kotlinexam.model.Contact
+import com.mineru.kotlinexam.viewmodel.ContactViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactViewModel: ContactViewModel
+
+    companion object {
+        val log = Logger.getLogger(MainActivity::class.java.name)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        log.info("OnCreate Called")
+
+        // Set contactItemClick & contactItemLongClick lambda
+        val adapter = ContactAdapter({ contact ->
+            // put extras of contact info & start AddActivity
+        }, { contact ->
+            deleteDialog(contact)
+        })
+
+        val lm = LinearLayoutManager(this)
+        main_recycleview.adapter = adapter
+        main_recycleview.layoutManager = lm
+        main_recycleview.setHasFixedSize(true)
+
+        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
+            adapter.setContacts(contacts!!)
+        })
+
+        main_button.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Delete selected contact?")
+            .setNegativeButton("NO") { _, _ -> }
+            .setPositiveButton("YES") { _, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        log.info("onStart Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        log.info("OnRestart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        log.info("OnResume Called")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        log.info("OnStop Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log.info("OnPause Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log.info("OnDestroy Called")
+    }
+}
