fb14c2fc9a28645f33c0ddab5f8380c8ccf2b40d
Mineru98 <rmstjr1030@naver.com>
2020-02-04 05:03:44 +0900

    Update Code

--- app/src/main/java/com/mineru/kotlinexam/view/MainActivity.kt
+++ app/src/main/java/com/mineru/kotlinexam/view/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        log.info("OnCreate Called")
 
         val adapter = ContactAdapter({ contact ->
             // put extras of contact info & start AddActivity
@@ -57,36 +56,5 @@ class MainActivity : AppCompatActivity() {
                 contactViewModel.delete(contact)
             }
         builder.show()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        log.info("onStart Called")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        log.info("OnRestart Called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        log.info("OnResume Called")
-    }
-
-
-    override fun onStop() {
-        super.onStop()
-        log.info("OnStop Called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        log.info("OnPause Called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        log.info("OnDestroy Called")
     }
 }
